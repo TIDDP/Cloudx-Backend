349026afb955840b81abd48e377d198442cd260f
Timi16 <timilehinolowu46@gmail.com>
2024-08-31 01:52:54 +0100

    Setting up server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 
 
 const app = express();
@@ -14,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Routes
 app.use('/api/users', userRoutes);
-
+app.use('/api/users', profileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
