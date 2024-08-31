6ff4a69b011745465bd0f3fd32d97024c750b6b2
Timi16 <timilehinolowu46@gmail.com>
2024-08-31 01:41:39 +0100

    Create student and aliumini get profile logic

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -29,3 +29,26 @@ exports.createAlumniProfile = async (req, res) => {
         res.status(500).json({ message: 'Server Error', error });
     }
 };
+
+// Get User Profile
+exports.getProfile = async (req, res) => {
+    try {
+        const userId = req.user.id; // Assume userId is available from JWT
+        let profile;
+
+        if (req.user.userType === 'student') {
+            profile = await StudentProfile.findOne({ userId }).populate('userId');
+        } else {
+            profile = await AlumniProfile.findOne({ userId }).populate('userId');
+        }
+
+        if (!profile) {
+            return res.status(404).json({ message: 'Profile not found' });
+        }
+
+        res.status(200).json(profile);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error });
+    }
+};
+
