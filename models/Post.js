c496a7c5ec59dbe42fa509790a8f1e5c1aa7d4d2
Timi16 <timilehinolowu46@gmail.com>
2024-09-04 11:25:56 +0100

    Adding Post feature

---
+++ models/Post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
