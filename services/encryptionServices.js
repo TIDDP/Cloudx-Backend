087c8024881665f4bc528148746061308f6089b4
Timi16 <timilehinolowu46@gmail.com>
2024-09-10 08:32:22 +0100

    Encrypting usermessages

---
+++ services/encryptionServices.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc'; 
+const secretKey = process.env.SECRET_KEY; // A 32-byte secret key
+const iv = crypto.randomBytes(16); // Initialization vector
+
+// Encrypt a message
+exports.encryptMessage = (message) => {
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
+    let encrypted = cipher.update(message);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
